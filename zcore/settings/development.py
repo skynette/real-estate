efd22a7c0b5fe04bd624f7f972d36337b27ad701
Joshua <cutejosh2@gmail.com>
2023-02-12 23:09:38 +0100

    added env.locale

--- zcore/settings/development.py
+++ zcore/settings/development.py
@@ -1,5 +1,15 @@
 from .base import *
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_USE_TLS = True
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = "info@realestate.com"
+DOMAIN = os.environ.get("DOMAIN")
+SITE_NAME = "Real Estate"
+
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.postgresql',
