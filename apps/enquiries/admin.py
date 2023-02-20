75c13313aad24bfc4377a05e04f9a2e5f69ae83d
Joshua <cutejosh2@gmail.com>
2023-02-20 22:03:20 +0100

    added enquiry model and admin

--- apps/enquiries/admin.py
+++ apps/enquiries/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Enquiry
 
-# Register your models here.
+
+class EnquiryAdmin(admin.ModelAdmin):
+	list_display = ['name', 'email', 'phone_number', 'subject']
+
+
+admin.site.register(Enquiry, EnquiryAdmin)
--- apps/enquiries/models.py
+++ apps/enquiries/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
+from apps.common.models import TimeStampedUUIDModel
 
-# Create your models here.
+class Enquiry(TimeStampedUUIDModel):
+	name = models.CharField(_("Your Name"), max_length=100)
+	phone_number = PhoneNumberField(_("Your Phone Number"), max_length=30, default="+2348189999999")
+	email = models.EmailField(_("Your Email"))
+	subject = models.CharField(_("Subject"), max_length=100)
+	message = models.TextField(_("Message"))
+
+	def __str__(self) -> str:
+		return f"{self.email}"
+
+	class Meta:
+		verbose_name_plural = "Enquiries"
