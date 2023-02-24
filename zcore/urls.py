017cd2fd967d69ce94f7a7b2a8cf2db9be232b63
Joshua <cutejosh2@gmail.com>
2023-02-25 00:45:34 +0100

    setup docker compose

--- zcore/urls.py
+++ zcore/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('swagger(<format>\.json|\.yaml)/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + staticfiles_urlpatterns()
+]
 
 
 admin.site.site_header = "Real Estate Admin"
