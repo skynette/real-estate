34ca541b08c99953526bd90ade735006c007f496
Joshua <cutejosh2@gmail.com>
2023-04-07 15:26:52 +0100

    celery worker issues

--- zcore/celery.py
+++ zcore/celery.py
@@ -8,7 +8,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zcore.settings.development")
 
 app = Celery("real_estate")
 
-app.config.from_object("zcore.settings.development", namespace="CELLERY")
+app.config_from_object("zcore.settings.development", namespace="CELLERY")
 
 app.autodiscover_tasks(lambda: base.INSTALLED_APPS)
 
